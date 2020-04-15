e6c7ca39657eb431bc92898fb4022d2018e5129b
pakkiz <63645383+pakkiz@users.noreply.github.com>
2020-04-15 15:36:17 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,16 +18,13 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-hoi {}, my name is {}! if you have any questions about how to use me please give me /help... 
+Hi {} ' My Name Is {} , People Call Me Esther.
 
-im a group manager bot maintained by  [this person](tg://user?id={}).
+I Can Never Help You.
 
-My future updates will be put into This Channel - @MarieChechi & My Support Group @InFoTelGroup.
+Smoke Is My Weekness🚭
 
-This is my [Deploy Code](https://heroku.com/deploy?template=https://github.com/TGExplore/Marie-2.0-English),
-you can create clone same like me..
-
-For more commands click /help...
+🔘Especially Created For @Cinema_Collections Group
 
 **Keep in mind that any changes you DO do to the source have to be on github, as per the license.**
 
